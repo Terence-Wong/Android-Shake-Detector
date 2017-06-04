fb41528051b239ab28e3c69f98b8a9688575ef03
Terence-Wong <terencewong.work@gmail.com>
2017-06-03 19:35:24 -0700

    Update ShakeDetector.java

--- ShakeDetector.java
+++ ShakeDetector.java
@@ -7,13 +7,6 @@ import android.hardware.SensorManager;
 
 public class ShakeDetector implements SensorEventListener {
 
-    /*
-     * The gForce that is necessary to register as shake.
-     * Must be greater than 1G (one earth gravity unit).
-     * You can install "G-Force", by Blake La Pierre
-     * from the Google Play Store and run it to see how
-     *  many G's it takes to register a shake
-     */
     private static final float SHAKE_THRESHOLD_GRAVITY = 2.7F;
     private static final int SHAKE_SLOP_TIME_MS = 500;
     private static final int SHAKE_COUNT_RESET_TIME_MS = 3000;
